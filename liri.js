02856662284a807bdb94b78ae12aaf5cb0a98d10
Lappy Mc Lap Face <ephillipsiii>
2018-04-15 16:02:33 -0600

    holy debugging batman

--- liri.js
+++ liri.js
@@ -1,6 +1,6 @@
 // setting variables for global scope
 
-require("dotenv").config();
+var dotenv = require("dotenv").config();
 var keys = require("./keys.js");
 var fs = require('fs');
 var twitter = require('twitter');
@@ -31,6 +31,7 @@ switch (action) {
     break;
 }
 
+//twitter function
 function twitter(inputs) {
     var params = {screen_name: inputs, count: 20};
         client.get("statuses/user_timeline", params, function(error, tweets, response){
@@ -44,7 +45,7 @@ function twitter(inputs) {
             }
         })
 }
-
+// //spotify function
 function spotify(inputs) {
     var spotify = new Spotify(keys.spotify);
     if (!inputs){
@@ -60,6 +61,45 @@ function spotify(inputs) {
         console.log("Song Name: " + songData[0].name);
         console.log("Preview Link: " + songData[0].preview_url);
         console.log("Album: " + songData[0].album.name);;
+    })
+}
+//omdb function
+function movie(inputs){
+    var queryURL = "http://ww.omdbapi.com/?t=" + inputs + "&y=&plot=short&apikey=trilogy";
+
+    request(queryURL, function (error, response, body){
+        if (!inputs) {
+            inputs = "Mr. Nobody";
+        }
+        if(!error && response.statusCode === 200){
+            console.log("Title: " + JSON.parse(body).Title);
+            console.log("Year: " + JSON.parse(body).Year);
+            console.log("IMDB Rating: " + JSON.parse(body).imdbRating);
+            console.log("Rotten Tomatoes Rating: " + JSON.parse(body).Ratings[1].Value);
+            console.log("Country: " + JSON.parse(body).Country);
+            console.log("Language: " + JSON.parse(body).Language);
+            console.log("Plot: " + JSON.parse(body).Plot);
+            console.log("Actors: " + JSON.parse(body).Actors);
+        }
+    })
+}
+// doit function!
+function doIt(){
+    fs.appendFile('random.txt', utf8, function(error, data){
+        if (error) {
+            return console.log(error);
+        }
+        var dataArray = data.split(",");
+        if(dataArray[0] === "spotify-this-song"){
+            var songcheck = dataArray[1].slice(1, -1);
+            spotify(songcheck);
+        }else if (dataArray[0] === "my-tweets"){
+            var tweet = dataArray[1].slice(1, -1);
+            twitter(tweet);
+        }else if (dataArray[0] === "movie-this") {
+            var movieName = dataArray[1].slice(1, -1);
+            movie(movieName);
+        }
     })
 }
 
